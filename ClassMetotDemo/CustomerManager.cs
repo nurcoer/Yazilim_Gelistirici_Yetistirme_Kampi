0af6b408499f1c1bb220fd71015c6c697eb3e2bc
KadriyeNur <kadriyecoer@gmail.com>
2021-01-20 15:44:00 +0300

    v0.1

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        // musteri listesi
+       List<Customer> customers =new List<Customer>();
+
+
+        // musterileri listeleme
+        public List<Customer> GetCustomers()
+        {
+            return customers;
+        }
+
+
+        //musteri ekleme
+        public void AddToCustomer(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine(customer.Name + " adlı kişi Müşterilere eklendi");
+        }
+
+
+        //musteri silme
+        public List<Customer> RemovoFromCustomer(Customer customer)
+        {
+            Customer RemovedCustomer = customers.Find(c => c.Id == customer.Id);
+            //kullanıcının listenin içerisinde var olup olmadığını kontrol eder
+            if (RemovedCustomer.Name == "")
+            {
+                // yoksa  uyarır.
+                Console.WriteLine(" Kullanıcı zaten ekli değil.\n");
+            }
+            else
+            {
+                //varsa siler
+                customers.Remove(customer);
+                Console.WriteLine( RemovedCustomer.Name + " Adlı müşteri müşterilerden silindi.\n");
+            }
+            return customers;
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //Müşteriler
+            Customer c1 = new Customer()
+            {
+                Id = 1,
+                Name = "Nur",
+                LastName = "Cöer",
+            };
+            Customer c2 = new Customer()
+            {
+                Id = 1,
+                Name = "Türkan",
+                LastName = "Cöer",
+            };
+            Customer c3 = new Customer()
+            {
+                Id = 1,
+                Name = "Emre",
+                LastName = "Cöer",
+            };
+            Customer c4 = new Customer()
+            {
+                Id = 1,
+                Name = "Ali",
+                LastName = "Cöer",
+            };
+
+
+            //İşlemler
+            Console.WriteLine("\n\n------Ekleme-------\n\n");
+            customerManager.AddToCustomer(c1);
+            customerManager.AddToCustomer(c2);
+            customerManager.AddToCustomer(c3);
+            customerManager.AddToCustomer(c4);
+
+
+            Console.WriteLine("\n\n------Listeleme-------\n\n");
+            List<Customer> customers = customerManager.GetCustomers();
+            ListCustomer(customers);
+
+            Console.WriteLine("\n\n------Silme-------\n\n");
+            customers = customerManager.RemovoFromCustomer(c1);
+            ListCustomer(customers);
+
+            Console.ReadLine();
+
+            //Ekleme Funksiyonu
+            void AddCustomer(Customer c)
+            {
+                customerManager.AddToCustomer(c);
+            }
+
+
+            //Listeleme işlemini ekrana yazdıran fonksiyon
+            void ListCustomer(List<Customer> Lcustomers)
+            {
+                foreach (Customer customerItem in Lcustomers)
+                {
+                    Console.WriteLine(customerItem.Name);
+                }
+            }
+        }
+
+        
+    }
+}
